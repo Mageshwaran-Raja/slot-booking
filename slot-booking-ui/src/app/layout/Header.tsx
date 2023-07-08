388b869286f10bc5ba26afa34215358b0eee4367
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-07-08 14:13:58 +0530

    responsive styles added

--- slot-booking-ui/src/app/layout/Header.tsx
+++ slot-booking-ui/src/app/layout/Header.tsx
@@ -1,6 +1,32 @@
 import {useNavigate } from 'react-router-dom';
 import './Header.css'
+import { useEffect, useState } from 'react';
 export default function Header() {
+
+    // Responsive Style
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const scalingFactor = (screenWidth / 1920);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setScreenWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
+    const logoStyle = {
+        width: `206px * ${scalingFactor}`,
+        height: `27px * ${scalingFactor}`,
+        position: "absolute",
+        top: `30px * ${scalingFactor}`,
+        left: "5.20833%"
+    }
+
     const midLinks = [
         { title: 'Home', path: '/' },
         { title: 'Book space', path: '/book-room' },
@@ -11,8 +37,20 @@ export default function Header() {
     const navigate = useNavigate();
     return (
         <>
-            <div className="nav-bar">
-                <img className='logo' alt='kaninilogo' src='/images/Kanini-Workspace.svg' />
+            <div 
+                style={{
+                    height: `calc(84px * ${scalingFactor})`, 
+                    backgroundColor: "#EAE9EA"
+                }} 
+                className="nav-bar"
+            >
+                <img className='logo' style={{
+                    width: `calc(206px * ${scalingFactor})`,
+                    height: `calc(27px * ${scalingFactor})`,
+                    position: "absolute",
+                    top: `calc(30px * ${scalingFactor})`,
+                    left: "5.20833%"
+                }} alt='kaninilogo' src='/images/Kanini-Workspace.svg' />
                 <ul className='nav-bar-items'>
                     {midLinks.map(({ title, path }) => (
                         <li className='nav-bar-item' onClick={() => navigate(path)} key={path}>
